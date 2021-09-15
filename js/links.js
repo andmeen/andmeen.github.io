d51df1b94800e40ca667b5b22917d9eb14ce148a
dev1213a <86798006+dev1213a@users.noreply.github.com>
2021-09-15 10:46:49 +0300

    new site

---
+++ js/links.js
@@ -0,0 +1,16 @@
+var mobileMenuBtn = document.querySelector("#mobile-menu-btn");
+var mobileMenu = document.querySelector(".mobile-menu");
+mobileMenuBtn.addEventListener("click", () => {
+  if (mobileMenu.style.display === "none") {
+    mobileMenu.style.display = "flex";
+    mobileMenuBtn.innerHTML = "Close";
+  } 
+  else {
+    mobileMenu.style.display = "none";
+    mobileMenuBtn.innerHTML = "menu";
+  }
+});
+
+let myname = "My Name's Ameen Faisal";
+
+console.log(myname);
