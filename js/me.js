6e18cb43939a8391249b4d5ca93b53aa3ebff8fb
dev1213a <86798006+dev1213a@users.noreply.github.com>
2021-10-27 10:09:27 +0300

    edit name img

--- js/me.js
+++ js/me.js
@@ -1,70 +1,29 @@
-const form = document.getElementById("form");  
- const username = document.getElementById("username");  
- const email = document.getElementById("email");  
- const password = document.getElementById("password");  
- const cPassword = document.getElementById("c-password");  
- //Show Error Message  
- function showError(input, message) {  
-  const formControl = input.parentElement;  
-  formControl.className = "input error";  
-  const small = formControl.querySelector("small");  
-  small.innerText = message;  
- }  
- //Show Success message  
- function showSuccess(input) {  
-  const formControl = input.parentElement;  
-  formControl.classList.add("success");  
- }  
- //Check Required fields  
- function checkRequired(inputArr) {  
-  inputArr.forEach(function (input) {  
-   if (input.value.trim() === "") {  
-    showError(input, `${getFieldName(input)} is required`);  
-   } else {  
-    showSuccess(input);  
-   }  
-  });  
- }  
- // Get Field Name  
- function getFieldName(input) {  
-  return input.id.charAt(0).toUpperCase() + input.id.slice(1);  
- }  
- // Check Input Lenght  
- function checkLenghth(input, min, max) {  
-  if (input.value.length < min) {  
-   showError(  
-    input,  
-    `${getFieldName(input)} must be at least ${min} characters `  
-   );  
-  } else if (input.value.length > max) {  
-   showError(  
-    input,  
-    `${getFieldName(input)} must be less than ${max} characters `  
-   );  
-  } else {  
-   showSuccess(input);  
-  }  
- }  
- // Check E-mail Is Valid  
- function checkEmail(input) {  
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;  
-  if (re.test(input.value.trim())) {  
-   showSuccess(input);  
-  } else {  
-   showError(input, "E-mail is not Valid");  
-  }  
- }  
- // Check Password Match  
- function checkPasswordMatch(input1, input2) {  
-  if (input1.value !== input2.value) {  
-   showError(input2, "Passwords do not match");  
-  }  
- }  
- form.addEventListener("submit", (e) => {  
-  e.preventDefault();  
-  checkRequired([username, email, password, cPassword]);  
-  checkLenghth(username, 3, 15);  
-  checkLenghth(password, 8, 25);  
-  checkEmail(email);  
-  checkPasswordMatch(password, cPassword);  
- });
+
+}
+$(document).ready(function(){
+	setTimeout(function(){ test(); });
+});
+$(window).on('resize', function(){
+	setTimeout(function(){ test(); }, 500);
+});
+$(".navbar-toggler").click(function(){
+	$(".navbar-collapse").slideToggle(300);
+	setTimeout(function(){ test(); });
+});
+
+
+
+// --------------add active class-on another-page move----------
+jQuery(document).ready(function($){
+	// Get current path and find target link
+	var path = window.location.pathname.split("/").pop();
+
+	// Account for home page with empty path
+	if ( path == '' ) {
+		path = 'index.html';
+	}
+
+	var target = $('#navbarSupportedContent ul li a[href="'+path+'"]');
+	// Add active class to target link
+	target.parent().addClass('active');
+});
