5df625856b342ee73928c3525de0438f0b9d119b
Ameen1211 <87598580+andmeen@users.noreply.github.com>
2022-05-11 19:31:16 +0300

    add some edit

--- js/javascript.js
+++ js/javascript.js
@@ -12,15 +12,3 @@ hamburger.addEventListener('click', ()=>{
     //Hamburger Animation
     hamburger.classList.toggle("toggle");
 });
-
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-    document.body.style.backgroundColor = "white";
-  }
