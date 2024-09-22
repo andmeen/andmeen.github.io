6e0ce5a0dbac080cb8cd5aff76f19eb0998fe855
Ameen1211 <87598580+andmeen@users.noreply.github.com>
2024-09-22 22:33:09 +0300

    loader

--- js/script.js
+++ js/script.js
@@ -9,3 +9,15 @@ const navLinks = document.querySelectorAll(".nav-item").forEach((navItem) => {
     document.querySelector(".nav-items")?.classList.toggle("active");
   });
 });
+
+
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 750);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
