c85974cc1743a0682ce9fc7c0a0dbc01dd1ff2ba
Ameen1211 <87598580+andmeen@users.noreply.github.com>
2024-10-02 18:49:47 +0300

    DATA PAGE

---
+++ data.js
@@ -0,0 +1,10 @@
+const lan_data = ["HTML", "CSS", "JAVASCRIPT", "FLUTTER('We Don't Have It Yet')"];
+
+
+const count_ofvideos = ["HTML= 4 videos", "CSS= 26 videos", "JAVASCRIPT= 42 videos", "FLUTTER= 0 video"];
+
+const pages = ["We Have 8 Pages"];
+
+
+
+document.write('<h1 id="jstext">' +lan_data,count_ofvideos, pages + '</h1>');
