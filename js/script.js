6761166cc464535a93f4d8ecde4dd932a6893933
Ameen1211 <87598580+andmeen@users.noreply.github.com>
2024-10-04 10:30:27 +0300

    host improvemnt

--- js/script.js
+++ js/script.js
@@ -32,25 +32,43 @@ document.addEventListener("DOMContentLoaded", (event) => {
 });
 
 
-// Function to display the images
-function currentImage(n) {
-    var i;
-    // Get all elements with the class name "imageClass"
-    var images = document.getElementsByClassName("imageClass");
-    // Get all elements with the class name "dot" (navigation dots)
-    var dots = document.getElementsByClassName("dot");
-    for (i = 0; i < images.length; i++) {
-        images[i].style.display = "none";
-    }
-    // Remove the "active" class from all navigation dots
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    // Display the image corresponding to the current index
-    images[n - 1].style.display = "block";
-    // Add the "active" class to the corresponding navigation dot
-    dots[n - 1].className += " active";
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
 }
 
-// Display the initial image
-currentImage(1);
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  // Hide all slides
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  // Remove "active" class from all dots
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  // Display the current slide and add "active" class to the corresponding dot
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
