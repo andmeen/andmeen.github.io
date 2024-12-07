574459c79b63babd304bb5e02a7283c5b4b6bc3f
Ameen1211 <87598580+andmeen@users.noreply.github.com>
2024-12-07 10:22:28 +0300

    some edits

--- js/script.js
+++ js/script.js
@@ -86,3 +86,19 @@ document.addEventListener('DOMContentLoaded', () => {
     showInstagramToast();
 });
 
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const elements = document.querySelectorAll(".fade-in");
+
+//   const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//       if (entry.isIntersecting) {
+//         entry.target.classList.add("visible");
+//       }
+//     });
+//   });
+
+//   elements.forEach((element) => {
+//     observer.observe(element);
+//   });
+// });
